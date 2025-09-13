fee2f6d3211f5b2e6836b9fae9b2d5ff80345c73
abenrahou <abenrahou05@gmail.com>
2025-09-13 16:16:51 +0200

    update settings.py

--- schmidtproject/settings.py
+++ schmidtproject/settings.py
@@ -118,16 +118,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL  = '/media/'
 STATICFILES_DIRS = [ BASE_DIR / 'schmidt_app' / 'static' ]
+STATIC_ROOT = '/var/www/schmidt_app/static'
+MEDIA_ROOT  = '/var/www/schmidt_app/media'
 
-STATIC_ROOT = BASE_DIR / "static"
-
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
